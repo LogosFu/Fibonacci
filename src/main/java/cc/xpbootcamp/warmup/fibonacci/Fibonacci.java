cb20fb2bcb618e1fd2d86efd2b8fc4efcd476fdb
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-02-15 19:21:22 +0800

    :sparkles: given 2 return 1 and given 3 return 2

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -3,6 +3,9 @@ package cc.xpbootcamp.warmup.fibonacci;
 public class Fibonacci {
 
   public int calculate(int index) {
+    if (index >2){
+      return 2;
+    }
     return 1;
   }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -12,4 +12,18 @@ class FibonacciTest {
     assertThat(fibonacciNumber).isEqualTo(1);
   }
 
+  @Test
+  void should_return_1_when_calculate_given_index_2() {
+    final Fibonacci fibonacci = new Fibonacci();
+    final int fibonacciNumber = fibonacci.calculate(2);
+    assertThat(fibonacciNumber).isEqualTo(1);
+  }
+
+  @Test
+  void should_return_2_when_calculate_given_index_3() {
+    final Fibonacci fibonacci = new Fibonacci();
+    final int fibonacciNumber = fibonacci.calculate(3);
+    assertThat(fibonacciNumber).isEqualTo(2);
+  }
+
 }
